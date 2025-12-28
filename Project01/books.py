ee0e64e61ef5bccb0947d240e10f114b81c78938
Upendar Boothkuru <mounikareddybeerelli@gmail.com>
2025-12-27 21:44:27 -0600

    Read specific book data using GET methodquery parameters

--- Project01/books.py
+++ Project01/books.py
@@ -7,7 +7,9 @@ books = [
     {"title": "The Pragmatic Programmer", "author": "Andrew Hunt, David Thomas", "category": "Software Engineering"},
     {"title": "You Don't Know JS", "author": "Kyle Simpson", "category": "JavaScript"},
     {"title": "Design Patterns", "author": "Erich Gamma et al.", "category": "Software Architecture"},
-    {"title": "Introduction to Algorithms", "author": "Thomas H. Cormen", "category": "Computer Science"}
+    {"title": "Introduction to Algorithms", "author": "Thomas H. Cormen", "category": "Computer Science"},
+    {"title": "You Know TS", "author": "Kyle Simpson", "category": "TypeScript"},
+
 ]
 
 # -------- GET message ----------
@@ -36,6 +38,8 @@ async def read_all_books() :
         "data" : books
     }
 # -------- GET the specific book ----------
+
+# path parameters
 @app.get("/books/{book_title}")
 async def read_single_book(book_title: str) :
     for book in books :
@@ -43,3 +47,21 @@ async def read_single_book(book_title: str) :
         if book.get('title').lower() == book_title.lower() :
             return book
 
+# query parameters
+@app.get("/books/")
+async def read_category_by_query(category : str) :
+    book_to_return = []
+    for book in books :
+        if book.get('category').lower() == category.lower() :
+            book_to_return.append(book)
+    return book_to_return
+        
+@app.get("/books/{book_author}/")
+async def read_author_category_by_query(book_author : str, category: str) :
+    books_to_return = []
+    for book in books :
+        if book.get('author').lower() == book_author.lower() and \
+            book.get('category').lower() == category.lower() :
+                books_to_return.append(book)
+                
+    return books_to_return
--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 ### What are Path Parameters ?
 - Path Parameters are request parameters that have been attached to the URL.
 - Path parameters are usually defined as a way to find information based on location.
+
+### What are Query Parameters ?
+- Query Parameters are request parameters that have beeen attached after a `?`.
+- Query parameters have `name=value` pairs
+- Example :
+    - 127.0.0.1:8000/books/?category=math
+
