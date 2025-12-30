3af37596c8a3e404d03c17bca9fa76a2a5f45758
Upendar Boothkuru <mounikareddybeerelli@gmail.com>
2025-12-30 10:29:48 -0600

    Get all todos from db

--- Project03/TodoApp/main.py
+++ Project03/TodoApp/main.py
@@ -1,11 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 import models
-from database import engine 
+from models import Todos
+from database import engine, sessionLocal
+from typing import Annotated
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind = engine)
 
-@app.get("/")
-def root():
-    return {"status": "Todo API running"}
+def get_db() :
+    db = sessionLocal()
+    try :
+        yield db
+    finally :
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+      
+@app.get("/todos")
+async def read_all_todos(db: db_dependency) :
+    return db.query(Todos).all()
