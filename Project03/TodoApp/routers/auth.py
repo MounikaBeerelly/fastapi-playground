e601e22531100d7756ca665a43169a174a6c419d
Upendar Boothkuru <mounikareddybeerelli@gmail.com>
2026-01-01 23:23:59 -0600

    Decoding JWT token

--- Project03/TodoApp/routers/auth.py
+++ Project03/TodoApp/routers/auth.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from pydantic import BaseModel
 from models import Users
 from passlib.context import CryptContext
@@ -6,8 +6,8 @@ from database import sessionLocal
 from typing import Annotated
 from sqlalchemy.orm import Session
 from starlette import status
-from fastapi.security import OAuth2PasswordRequestForm
-from jose import jwt
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from jose import JWTError, jwt
 from datetime import timedelta, datetime, timezone
 
 router = APIRouter(
@@ -23,6 +23,8 @@ bcrypt_context = CryptContext(
     schemes = ["bcrypt"], 
     deprecated = "auto"
     )
+
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl = 'token')
 
 def get_db() :
     db = sessionLocal()
@@ -41,6 +43,7 @@ def authenticated_user(username : str, password : str, db) :
         return False
     return user
 
+# Encoding JWT token
 def create_access_token(username : str, user_id : int, expires_delta : timedelta) :
     encode = {
         'sub' : username,
@@ -49,7 +52,23 @@ def create_access_token(username : str, user_id : int, expires_delta : timedelta
     expires = datetime.now(timezone.utc) + expires_delta
     encode.update({'exp' : expires})
     return jwt.encode(encode, SECRET_KEY, algorithm = ALGORITHM)
-        
+
+# Decoding JWT token
+async def get_current_user(token : Annotated[str, Depends(oauth2_bearer)]) :
+    try :
+        payload = jwt.decode(token, SECRET_KEY, algorithms = [ALGORITHM])
+        username : str = payload.get('sub')
+        user_id : int = payload.get('id')
+        if username is None or user_id is None :
+            raise HTTPEXception(status_code = status.HTTP_401_UNAUTHORIZED, detail = 'Could not validate user..')
+        return {
+            'username' : username,
+            'id' : user_id
+        }
+    except JWTError :
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail = 'Could not Validate user...')
+    
+    
 class CreateUserRequest(BaseModel) :
     username : str
     email : str
