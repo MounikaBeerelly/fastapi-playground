1a8680180507d16542ceed720ba5513581c867e5
Upendar Boothkuru <mounikareddybeerelli@gmail.com>
2026-01-03 23:34:56 -0600

    Alembic downgrade

---
+++ Assignments/assignment04.py
@@ -0,0 +1,4 @@
+"""
+Add a phone number field as required when we create a new user within our auth.py file
+Create a new @put request in our users.py file that allows a user to update their phone_number
+"""
